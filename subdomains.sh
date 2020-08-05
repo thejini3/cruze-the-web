a858d572077c4f6805de8d0a5e6346e963706427
Your Name <you@example.com>
2020-08-05 09:33:13 +0600

    changes, meow

--- subdomains.sh
+++ subdomains.sh
@@ -14,22 +14,23 @@ if [ "$dir" != "." ]; then
 	mkdir -p $dir
 fi
 
-echo '-------------------Assetfinder start------------------------------------------'
+echo '\n########################### Assetfinder Start ###########################\n'
 assetfinder --subs-only $domain | tee $dir/asset_subs.txt
+echo '\n########################### Assetfinder end ###########################\n'
 
 #Sublister
-echo $'\n-------------------Sublister start------------------------------------------'
+echo '\n########################### Sublister start ###########################\n'
 python3 ~/tools/Sublist3r/sublist3r.py -v -t 10 -d $domain -o $dir/subs.txt
-echo $'-------------------Sublister end---------------------\n'
+echo '\n########################### Sublister end ###########################\n'
 
 cat $dir/asset_subs.txt $dir/subs.txt | sort -u > $dir/subdomains.txt
 rm $dir/asset_subs.txt
 rm $dir/subs.txt
 
-echo $'\nTotal number of subdomains\n'
+echo '\n########################### Total number of subdomains ###########################\n'
 cat  $dir/subdomains.txt | wc -l
 
-echo $'\n-------------------httprobe start-------------------\n'
-cat $dir/subdomains.txt | httprobe -c 50 -t 3000 > $dir/live_subdomains.txt
+echo '\n########################### httprobe start ###########################\n'
+cat $dir/subdomains.txt | httprobe | tee $dir/live_subdomains.txt
 cat $dir/live_subdomains.txt | sort -u | tee $dir/live_subdomains.txt
-echo $'-------------------httprobe end-------------------\n'
+echo '\n########################### httprobe end ###########################\n'
